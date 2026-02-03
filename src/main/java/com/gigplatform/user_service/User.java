73323408064275777a3897522ef25519fd79a745
Aashi <Ashishmaurya9193@gmail.com>
2026-02-02 16:49:17 -0800

    added user Authentication

--- src/main/java/com/gigplatform/user_service/User.java
+++ src/main/java/com/gigplatform/user_service/User.java
@@ -2,12 +2,20 @@ package com.gigplatform.user_service;
 
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "users")
 public class User {
@@ -17,8 +25,17 @@ public class User {
 	private Long id;
 
 	private String name;
+
+	@Column(unique = true, nullable = false)
 	private String email;
+
+	@JsonIgnore
 	private String password;
+
+	@Enumerated(EnumType.STRING)
+	private AuthProvider provider;
+
+	private String providerId;
 
 	public User() {
 	}
@@ -30,35 +47,13 @@ public class User {
 		this.password = password;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
+	public User(Long id, String name, String email, String password, AuthProvider provider, String providerId) {
 		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
 		this.password = password;
+		this.provider = provider;
+		this.providerId = providerId;
 	}
+
 }
